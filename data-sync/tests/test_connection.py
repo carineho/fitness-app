2c8bdb851771c8ebbef43e50e80468a07b8e8989
Ho Kah Hsin Carine <carinehokh@gmail.com>
2026-08-03 17:45:15 +0530

    added commands to purge all data in DB tables

--- data-sync/tests/test_connection.py
+++ data-sync/tests/test_connection.py
@@ -12,6 +12,19 @@ if not DATABASE_URL:
 
 conn = psycopg2.connect(DATABASE_URL, connect_timeout=5)
 cur = conn.cursor()
+
+# Purge all data in the tables
+cur.execute("DELETE FROM climb; DELETE FROM climbsession;")
+cur.execute("DELETE FROM exercise; DELETE FROM strengthsession;")
+cur.execute("DELETE FROM rundetail; DELETE FROM yogadetail; DELETE FROM divedetail;")
+cur.execute("DELETE FROM activity;")
+conn.commit()
+
+# Count the number of rows in Activity table
+cur.execute("SELECT COUNT(*) FROM activity;")
+
+# Execute simple command to get SQL version
 cur.execute("SELECT version();")
+
 print(cur.fetchone())
 conn.close()
